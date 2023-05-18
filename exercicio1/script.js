f424a584ca3ee6b8e3eddc74465e6a44e16ec653
LCassio99 <lucianocassio66@gmail.com>
2023-05-17 21:53:18 -0300

    exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+const objeto1 = {
+  fruta1: 'Laranja',
+  precoFruta1: 2,
+  disponibilidadeFruta1: true
+}
+
+const objeto2 ={
+  fruta2:'Pequi',
+  precoFruta2: 8,
+  disponibilidadeFruta2: false
+}
+
+const objeto3 ={
+  fruta3:'Umbu',
+  precoFruta3: 4,
+  disponibilidadeFruta2: true
+}
+
+const sacolao = []
+sacolao.push(objeto1,objeto2,objeto3)
+console.log(sacolao)
