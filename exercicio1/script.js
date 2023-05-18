04c3ae0b65b0f6eed3452896d4c31d4f6dca240f
LCassio99 <lucianocassio66@gmail.com>
2023-05-17 22:13:28 -0300

    exercicio2 + correcao exercicio1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,21 +1,19 @@
-const objeto1 = {
+const objetos = [{
   fruta1: 'Laranja',
   precoFruta1: 2,
   disponibilidadeFruta1: true
-}
-
-const objeto2 ={
+},
+{
   fruta2:'Pequi',
   precoFruta2: 8,
   disponibilidadeFruta2: false
-}
-
-const objeto3 ={
+},
+{
   fruta3:'Umbu',
   precoFruta3: 4,
   disponibilidadeFruta2: true
 }
-
+]
 const sacolao = []
-sacolao.push(objeto1,objeto2,objeto3)
+sacolao.push(objetos)
 console.log(sacolao)
